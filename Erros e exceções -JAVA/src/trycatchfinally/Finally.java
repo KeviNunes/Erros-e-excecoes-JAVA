ef4fc3222f1c5df7c3ad1be2159bb6073f9832fd
Kevin Nunes <58992523+KeviNunes@users.noreply.github.com>
2021-03-05 08:27:20 -0300

    Update Finally.java

--- Erros e exceções -JAVA/src/trycatchfinally/Finally.java
+++ Erros e exceções -JAVA/src/trycatchfinally/Finally.java
@@ -5,7 +5,7 @@ public class Finally {
 	public static void main(String[] args) {
 		// Declaração dos arrays
 		int[] num = {4,8,18,32,64,128};	// Array com os numeradores
-		int[] deno = {2,0,4,8,0};		// Array com os denominadores
+		int[] deno = {2,0,4,8,0};	// Array com os denominadores
 		
 		for(int i=0; i<num.length; ++i) {
 			
@@ -22,7 +22,7 @@ public class Finally {
 			catch(Throwable exe) {	// Exceção genérica
 				System.out.println("Houve um erro");
 			}
-			finally{ // Codigo executado idependentemente de erros ou exceções
+			finally{ // Codigo executado independentemente de erros ou exceções
 				System.out.println("Terminando a execução do bloco try");
 				System.out.println();
 			}
